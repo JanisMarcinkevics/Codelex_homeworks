3a397662772e9120dbd25f0874726b6855864b9c
JanisMarcinkevics <101697377+JanisMarcinkevics@users.noreply.github.com>
2023-03-21 16:49:00 +0200

    Delete 01_VariablesAndDataTypes directory

--- 01_VariablesAndDataTypes/exercise_2.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$integer = 10;
-$float = 10.10;
-$string = 'hello world';
-
-var_dump($integer, $float, $string);
--- 01_VariablesAndDataTypes/exercise_3.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$name = "Jānis";
-$surname = "Marcinkevičs";
-$age = 43;
-
-echo $name . ' ' . $surname . ' ' . $age;
