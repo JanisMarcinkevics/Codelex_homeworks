3f2adb979bed6175407fb147b145facebb274fb2
JanisMarcinkevics <101697377+JanisMarcinkevics@users.noreply.github.com>
2023-03-21 16:49:39 +0200

    Delete 04_loops directory

--- 04_loops/exercise_1.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$Array = [1,8,3,4,5,6];
-
-foreach ($Array as $number) {
-    echo $number . "\n";
-}
--- 04_loops/exercise_2.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$array = [1,8,3,4,5,6];
-
-for ($number = 0;  $number < count($array); $number++) {
-    echo $array[$number] . "\n";
-}
