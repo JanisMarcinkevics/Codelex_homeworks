ae788826676fbb03a4d4a2170bcbf894ccfb5017
Jānis Marcinkevičs <janis.marcinkevics@gmail.com>
2023-03-21 14:59:26 +0200

    added comments

---
+++ 01_VariablesAndDataTypes/exercise_1.php
@@ -0,0 +1,11 @@
+<?php
+//Create variable that prints out an integer 10, float 10.10, string "hello world"
+
+
+$integer = 10;
+$float = 10.10;
+$string = 'hello world';
+
+$printAll = "$integer\n$float\n$string\n";
+
+echo $printAll;
