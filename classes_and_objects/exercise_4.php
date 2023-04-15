d34919f836a5b3fb9a87e9b6f72059c35e9a39b7
JanisMarcinkevics <janis.marcinkevics@gmail.com>
2023-04-15 16:12:54 +0300

    updated file

--- classes_and_objects/exercise_4.php
+++ classes_and_objects/exercise_4.php
@@ -12,21 +12,90 @@ class Movie
         $this->rating = $rating;
     }
 
-    public function getPG(array $movies): array
+    public function getTitle(): string
     {
-        return array_filter($movies, function($movie)
-        {
-            return $movie->rating === 'PG';
-        });
+        return $this->title;
+    }
+
+    public function getStudio(): string
+    {
+        return $this->studio;
+    }
+
+    public function getRating(): string
+    {
+        return $this->rating;
     }
 }
 
-$movies =
+class Cinema
+{
+    private array $movies;
+
+    public function __construct(array $movies)
+    {
+        foreach ($movies as $movie)
+        {
+            $this->addMovie($movie);
+        }
+    }
+
+    public function getMovies(): array
+    {
+        return $this->movies;
+    }
+
+    private function addMovie(Movie $movie): void
+    {
+        $this->movies[] = $movie;
+    }
+
+    public function getMoviesByRating(string $rating): array
+    {
+        $ratedMovies = [];
+        foreach($this->movies as $movie)
+        {
+            if($movie->getRating() == $rating)
+            {
+                $ratedMovies[] = $movie;
+            }
+        }
+        return $ratedMovies;
+    }
+}
+
+$cinema = new Cinema(
     [
         new Movie("Casino Royale", "Eon Productions", "PG-13"),
         new Movie("Glass", "Buena Vista International", "PG-13"),
         new Movie("Spider-Man: Into the Spider-Verse", "Columbia Pictures", "PG"),
-    ];
+    ]
+);
 
-return Movie::getPG($movies);
+
+echo "     Welcome to CODELEX Cinema \n";
+echo "------------------------------------- \n";
+echo "Today you can watch following movies: \n";
+
+
+foreach ($cinema->getMovies() as $movie)
+{
+    echo "{$movie->getTitle()} from studio {$movie->getStudio()} \n";
+}
+$userSelectedRating = readline('What rating movies would you like to see? ');
+$selectedMovies = $cinema->getMoviesByRating($userSelectedRating);
+
+
+if(empty($selectedMovies))
+{
+    exit ("There are no movies in this category.\n");
+}
+
+echo "You can enjoy following movies with {$userSelectedRating } rating: \n";
+
+foreach ($selectedMovies as $movie)
+{
+    echo "{$movie->getTitle()} from studio {$movie->getStudio()} \n";
+}
+
 
